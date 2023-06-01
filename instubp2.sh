b68d996bf386a3f6451b610fb11815dc8ebde931
Flavio Del Mestre <flavio@perunis.ch>
2023-06-01 12:07:29 +0200

    Correct Python3.11 install

--- instubp2.sh
+++ instubp2.sh
@@ -312,10 +312,11 @@ systemctl restart php8.2-fpm.service
 echo | add-apt-repository ppa:deadsnakes/ppa -y
 apt update
 apt install python3.11-full -y
+apt install python3-pip -y
 apt install python3.11-venv -y
 echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 2
-echo 2 > update-alternatives --config python3
+echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+echo 2 | update-alternatives --config python3
 
 
 ##############################
