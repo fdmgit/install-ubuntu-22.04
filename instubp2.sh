968b51f0c12cbb785a08388c929dbf4b417d8a0a
Flavio Del Mestre <flavio@perunis.ch>
2023-06-01 13:09:21 +0200

    Update instubp2.sh

--- instubp2.sh
+++ instubp2.sh
@@ -311,12 +311,12 @@ systemctl restart php8.2-fpm.service
 
 echo | add-apt-repository ppa:deadsnakes/ppa -y
 apt update
-apt install python3.11-full -y
-apt install python3.11-pip -y
+apt install python3.11 -y
 apt install python3.11-venv -y
 echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
 echo 2 | update-alternatives --config python3
+apt install python3-pip -y
 
 
 ##############################
