28dadba6ef0ce05180582a4db3459c6af24170ac
Flavio Del Mestre <flavio@perunis.ch>
2023-06-03 17:38:25 +0200

    Create load_file.sh

---
+++ load_file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/bashrc.ini
+wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/instubp1arm.sh
+wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/instubp2.sh
+
+cp bashrc.ini /root/.bashrc
+rm /root/bashrc.ini
+
+chmod +x /root/instubp1arm.sh
+chmod +x /root/instubp2.sh
+
