d8f8a2ecb4a1c7d828d2e00fe7acb2b623c9ef92
Flavio Del Mestre <flavio@perunis.ch>
2023-06-03 18:33:01 +0200

    Update load_inst_files_arm.sh

--- load_inst_files_arm.sh
+++ load_inst_files_arm.sh
@@ -2,11 +2,11 @@
 
 wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/bashrc.ini
 wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/instubp1arm.sh
-wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/instubp2.sh
+wget https://raw.githubusercontent.com/fdmgit/install-ubuntu-22.04/main/instubp2arm.sh
 
 cp bashrc.ini /root/.bashrc
 rm /root/bashrc.ini
 
 chmod +x /root/instubp1arm.sh
-chmod +x /root/instubp2.sh
+chmod +x /root/instubp2arm.sh
 
