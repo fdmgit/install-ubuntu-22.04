579ae2bd8835e2ba77ff2d44ebb1dafdf4b1393b
Flavio Del Mestre <flavio@perunis.ch>
2023-06-03 17:31:12 +0200

    Update instubp1arm.sh

--- instubp1arm.sh
+++ instubp1arm.sh
@@ -10,11 +10,6 @@ apt upgrade -y
 apt install plocate sntp ntpdate software-properties-common -y 
 hostnamectl set-hostname $2
 
-##############################
-#### Change MariaDB repository
-##############################
-#apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-#add-apt-repository 'deb [arch=ppc64el,amd64,arm64] https://mirror.mva-n.net/mariadb/repo/10.5/ubuntu focal main' 
 
 apt update 
 #passwd root
