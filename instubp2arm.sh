40d5810d36655d087f7cf79baab6e09a33c12fb3
Flavio Del Mestre <flavio@perunis.ch>
2023-06-04 16:19:16 +0200

    Update instubp2arm.sh

--- instubp2arm.sh
+++ instubp2arm.sh
@@ -309,15 +309,15 @@ systemctl restart php8.2-fpm.service
 #### install new Python versions
 ################################
 
-echo | add-apt-repository ppa:deadsnakes/ppa -y
-apt update
-apt install python3.11 -y
-apt install python3.11-venv -y
-echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
-echo 2 | update-alternatives --config python3
-apt install python3-pip -y
-echo | ln -s /usr/lib/python3/dist-packages/apt_pkg.cpython-310-aarch64-linux-gnu.so /usr/lib/python3/dist-packages/apt_pkg.so
+#echo | add-apt-repository ppa:deadsnakes/ppa -y
+#apt update
+#apt install python3.11 -y
+#apt install python3.11-venv -y
+#echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+#echo | update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+#echo 2 | update-alternatives --config python3
+#apt install python3-pip -y
+#echo | ln -s /usr/lib/python3/dist-packages/apt_pkg.cpython-310-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_pkg.so
 
 
 ##############################
