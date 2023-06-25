562575c59b556a1cbc42bc494410e3fe8cd5526a
Flavio Del Mestre <flavio@perunis.ch>
2023-06-25 08:12:38 +0200

    Update instubp1arm.sh

--- instubp1arm.sh
+++ instubp1arm.sh
@@ -21,7 +21,7 @@ echo "root:$1" | chpasswd   # set root password -
 ###################################
 
 cd /usr/local/bin
-wget https://github.com/koki-develop/gat/releases/download/v0.8.2/gat_Linux_arm64.tar.gz
+wget https://github.com/koki-develop/gat/releases/download/v0.8.3/gat_Linux_arm64.tar.gz
 tar -xvzf gat_Linux_arm64.tar.gz
 chown root:root gat
 chmod +x gat
