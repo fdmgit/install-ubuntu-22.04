d094534a0f13dcff1a85fa51add66fc6a29d892f
Flavio Del Mestre <flavio@perunis.ch>
2023-08-26 18:11:07 +0200

    Update load_inst_files.sh

--- load_inst_files.sh
+++ load_inst_files.sh
@@ -10,4 +10,21 @@ rm /root/bashrc.ini
 chmod +x /root/instubp1.sh
 chmod +x /root/instubp2.sh
 
+###################################
+#### Setup root key file
+###################################
 
+if [ -d /root/.ssh ]; then 
+    echo ".ssh exists"
+else
+    mkdir /root/.ssh
+fi
+
+if [ -f /root/.ssh/authorized_keys ]; then
+    echo "file authorized_keys exists"
+else
+    cd /root/.ssh
+    wget https://raw.githubusercontent.com/fdmgit/virtualmin/main/authorized_keys
+fi
+
+reboot
